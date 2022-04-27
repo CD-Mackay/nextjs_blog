09198742740e94d91d035e8807405dd722707344
Connor Mackay <connor.mackay@gmail.com>
2022-04-27 13:30:51 -0400

    build hero

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,5 +1,12 @@
+import classes from "./hero.module.css";
+import Image from "next/image";
+
 export default function Hero() {
   return (
-    <div>I am Hero</div>
-  )
-};
+    <section className={classes.hero}>
+      <div className={classes.image}><Image src="/../public/images/ongo-gablogian.jpg" alt="A man a plan a canal panama"  width={500} height={500} /></div>
+      <h1>Ongo Gablogian, The Art Collector</h1>
+      <p>Charmed I'm Sure</p>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import styles from "../styles/Home.module.css";
 import { Fragment } from "react";
+import Hero from "../components/home/Hero";
 
 export default function Home() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      {/* <FeaturedPosts /> */}
     </Fragment>
   );
 }
