b3d727430949a61562c30f04d3b73cad30164b14
Connor Mackay <connor.mackay@gmail.com>
2022-04-27 13:40:41 -0400

    build layout components

---
+++ components/layout/layout.js
@@ -0,0 +1,11 @@
+import navigation from './navigation';
+import { Fragment } from 'react';
+
+export default function Layout(props) {
+  return (
+    <Fragment>
+      <Navigation />
+      <main>{props.children}</main>
+    </Fragment>
+  )
+}
---
+++ components/layout/navigation.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+
+export default function Navigation() {
+return (
+  <header>
+    <Logo />
+    <nav>
+      <ul>
+        <li><Link></Link></li>
+      </ul>
+    </nav>
+  </header>
+  )
+}
