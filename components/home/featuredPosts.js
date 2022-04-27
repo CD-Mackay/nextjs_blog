571dec5d5774fbd53ac7474eb84b193e28972e5a
Connor Mackay <connor.mackay@gmail.com>
2022-04-27 13:55:26 -0400

    build featured page

---
+++ components/home/featuredPosts.js
@@ -0,0 +1,11 @@
+import classes from './featuredPosts.module.css';
+
+export default function FeaturedPosts() {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostList />
+      </section>
+
+  )
+};
