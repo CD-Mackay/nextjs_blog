4ec3880b5c4242d131232b5656203efa9dc6ce08
Connor Mackay <connor.mackay@gmail.com>
2022-04-28 14:35:56 -0400

    update dependencies'

--- components/posts/post-detail/postContent.js
+++ components/posts/post-detail/postContent.js
@@ -1,23 +1,23 @@
 import PostHeader from "./postHeader";
-import classes from './postContent.module.css';
+import classes from "./postContent.module.css";
+import ReactMarkdown from 'react-markdown';
+// const  ReactMarkdown  = require('react-markdown');
 
 export default function PostContent() {
+  const dummy_post = {
+    title: "A Crickets Tale",
+    image: "ongo-gablogian.jpg",
+    date: "2021-03-03",
+    slug: "a-crickets-tale",
+    content: "# This is the tale of Cricket",
+  };
 
-  const dummy_post = 
-    {
-      title: "A Crickets Tale",
-      image: "ongo-gablogian.jpg",
-      date: "2021-03-03",
-      slug: "a-crickets-tale",
-      content: "# This is the tale of Cricket"
-    };
+  const imagePath = `/images/${dummy_post.image}`;
 
-    const imagePath = `/images/${dummy_post.image}`
-  
-return (
-  <article className={classes.content}>
-    <PostHeader title={dummy_post.title} image={imagePath} />
-    {dummy_post.content}
-  </article>
-)
-};
+  return (
+    <article className={classes.content}>
+      <PostHeader title={dummy_post.title} image={imagePath} />
+      <ReactMarkdown>{dummy_post.content}</ReactMarkdown>
+    </article>
+  );
+}
