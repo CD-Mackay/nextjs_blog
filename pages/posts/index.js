e1279b3f3673534eb9d061c4e5f8a2e92ae918ff
Connor Mackay <connor.mackay@gmail.com>
2022-04-28 10:47:54 -0400

    build all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,40 @@
-export default function AllPosts() {
+import AllPosts from "../../components/posts/AllPosts"
+
+export default function AllPostsPage() {
+
+  const dummy_posts = [
+    {
+      title: "A Crickets Tale",
+      image: "ongo-gablogian.jpg",
+      excerpt: "The tale of Cricket",
+      date: "2021-03-03",
+      slug: "a-crickets-tale"
+    },
+    {
+      title: "The Gang Learns NextJS",
+      image: "ongo-gablogian.jpg",
+      excerpt: "The tale of Cricket",
+      date: "2021-03-03",
+      slug: "the-gang-learns-nextjs"
+    },
+    {
+      title: "Mac and Charlie: Web Devs",
+      image: "ongo-gablogian.jpg",
+      excerpt: "The tale of Cricket",
+      date: "2021-03-03",
+      slug: "mac-and-charlie-web-devs"
+    },
+    {
+      title: "Franks Back in Tech",
+      image: "ongo-gablogian.jpg",
+      excerpt: "The tale of Cricket",
+      date: "2021-03-03",
+      slug: "franks-back-in-tech"
+    },
+  
+  ]
+
   return (
-    <p>I am All AllPosts</p>
+    <AllPosts posts={dummy_posts} />
   )
 };
