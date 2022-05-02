cf7925aaa7d8ccef7c3ce4f273d5a13ed9c1e214
Connor Mackay <connor.mackay@gmail.com>
2022-05-02 14:59:17 -0400

    add head metadata

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,16 @@
-import ContactForm from "../components/contact/contactForm"
+import ContactForm from "../components/contact/contactForm";
+import { Fragment } from "react";
+import Head from "next/head";
+
 
 export default function Contact() {
   return (
-    <ContactForm /> 
+    <Fragment>
+      <Head>
+        <meta name="description" content="this is where you send us messages" />
+      </Head>
+          <ContactForm /> 
+
+    </Fragment>
   )
 };
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,17 @@ import { Fragment } from "react";
 import Hero from "../components/home/Hero";
 import FeaturedPosts from "../components/home/featuredPosts";
 import { getFeaturedPosts  } from "../helpers/posts-util";
+import Head from 'next/head';
 
 export default function Home(props) {
   const { posts } = props;
 
   return (
     <Fragment>
+      <Head>
+        <title>The ARt Blog</title>
+        <meta name="description" content="Ongos art blog" />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </Fragment>
