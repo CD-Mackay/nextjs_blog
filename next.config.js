09b5c22c72ad8ec03741c7643f36c3cd4dc6fe3b
Connor Mackay <connor.mackay@gmail.com>
2022-05-12 15:56:49 -0400

    add next config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    mongodb_username: 'ConnorTwo',
+    mongodb_password: 'ConnorPass',
+    mongodb_cluster: 'authcluster',
+    mongobd_database: 'contact-messages'
+  }
+}
