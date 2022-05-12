b0e35e4451adbdf966256bc36512dd136ef65e31
Connor Mackay <connor.mackay@gmail.com>
2022-05-12 16:15:29 -0400

    add variables for production vs development build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-module.exports = {
-  env: {
-    mongodb_username: 'ConnorTwo',
-    mongodb_password: 'ConnorPass',
-    mongodb_cluster: 'authcluster',
-    mongobd_database: 'contact-messages'
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "ConnorTwo",
+        mongodb_password: "ConnorPass",
+        mongodb_cluster: "authcluster",
+        mongobd_database: "contact-messages",
+      },
+    };
   }
-}
+};
