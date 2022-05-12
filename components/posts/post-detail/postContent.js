7e42c5e67cb267636993d5df02abc426ac144b0c
Connor Mackay <connor.mackay@gmail.com>
2022-05-12 16:56:02 -0400

    optimize syntax highlighter

--- components/posts/post-detail/postContent.js
+++ components/posts/post-detail/postContent.js
@@ -2,8 +2,11 @@ import PostHeader from "./postHeader";
 import classes from "./postContent.module.css";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+
+SyntaxHighlighter.registerLanguage('js', js);
 
 // const  ReactMarkdown  = require('react-markdown');
 
